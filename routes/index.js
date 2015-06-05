b866036e36f945e0f8fca0a1781d3de3f0df776d
Gaston Nan <gaston.nan@zedplan.com>
2015-06-05 20:48:08 -0300

    bower, socket.io del cliente, primeras lineas

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,20 @@
 var express = require('express');
+var http = require('http').Server( express() );
 var router = express.Router();
+var io = require('socket.io')(http); //esta instancia de http no es la q esta escuchando el puerto, talvez x eso no anda
+
+
+io.on('connection', function(socket){
+    console.log('a user connected');
+    socket.on('disconnect', function(){
+        console.log('user disconnected');
+    });
+});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'OpenFigths' });
+    res.render('index', { title: 'OpenFigths' });
 });
+
 
 module.exports = router;
